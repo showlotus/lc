6bc525ec71d6e98794438f7c87a2daf3d5d50c68
showlotus <7826116+showlotus@user.noreply.gitee.com>
2021-09-29 23:52:11 +0800

    9.29 reissue

--- 9.27-10.3/517.超级洗衣机.js
+++ 9.27-10.3/517.超级洗衣机.js
@@ -6,33 +6,21 @@ var findMinMoves = function (machines) {
   let sum = machines.reduce((prev, curr) => prev + curr)
   let average = sum / machines.length
   if (Number.isInteger(average)) {
-    let res = 0
-    machines.sort((a, b) => b - a)
-    while (1) {
-      if (machines.some(val => val !== average)) {
-        let moreThan = 0
-        for (let i = 0; i < machines.length; i++) {
-          if (machines[i] > average) {
-            moreThan += 1
-            machines[i] -= 1
-          } else if (machines[i] < average) {
-            if (moreThan > 0) {
-              machines[i] += 1
-              moreThan -= 1
-            }
-          }
-        }
-        res++
-        console.log(moreThan, machines, res)
-      } else {
-        return res
-      }
+    let diff = []
+    for (let i = 0; i < machines.length; i++) {
+      diff[i] = machines[i] - average
+    }
+    console.log(diff)
+    let res = Math.abs(diff[0])
+    for (let i = 1; i < diff.length; i++) {
+      res = Math.max(res, Math.abs(diff[i] + diff[i - 1]), diff[i])
+      diff[i] += diff[i - 1]
     }
     return res
   } else {
     return -1
   }
-};
+}
 
 let machines = [1, 0, 5, 2]
 
@@ -41,6 +29,9 @@ machines = [0, 3, 0, 5, 4, 0]
 machines = [1, 0, 5]
 machines = [4, 0, 0, 4]
 // machines = [1]
-machines = [1, 1, 1]
-machines = [0, 0, 11, 5]
+// machines = [1, 1, 1]
+// machines = [0, 0, 11, 5, 0, 2]
+// machines = [0, 0, 11, 5]
+// machines = [11, 0, 5, 0]
+machines = [9, 1, 8, 8, 9]
 console.log(findMinMoves(machines))
