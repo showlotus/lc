caa2feca464847fe958ce2290b09d2f5934bb82d
yaoxu <yaoxuu@qq.com>
2022-01-03 14:22:53 +0800

    1.3 ac

---
+++ 22 1.3-1.9/1185.一周中的第几天.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1185 lang=javascript
+ *
+ * [1185] 一周中的第几天
+ */
+
+// @lc code=start
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+const WeekList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var dayOfTheWeek = function (day, month, year) {
+  let weekDay = new Date(year, month - 1, day).getDay()
+  return WeekList[weekDay]
+}
+// @lc code=end
+var day = 31,
+  month = 8,
+  year = 2019
+
+// ;(day = 3), (month = 1), (year = 2022)
+
+console.log(dayOfTheWeek(day, month, year))
