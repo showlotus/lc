2510240ed6b03bbb932871265f3d95641652b3c1
yaoxu <yaoxuu@qq.com>
2022-01-08 20:33:06 +0800

    1.8 ac

---
+++ 22 1.3-1.9/11.盛最多水的容器.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0
+  let left = 0
+  let right = height.length - 1
+  while (left < right) {
+    max = Math.max(max, Math.min(height[left], height[right]) * (right - left))
+    if (height[left] <= height[right]) {
+      left++
+    } else {
+      right--
+    }
+  }
+  return max
+}
+// @lc code=end
+
+var height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+// height = [1, 0]
+
+console.log(maxArea(height))
