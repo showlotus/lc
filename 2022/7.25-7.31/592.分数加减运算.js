a55f875e60b7e39420df5807396df4688f9afc2b
showlotus <yaoxuu@qq.com>
2022-07-27 08:08:34 +0800

    7.27 ac

---
+++ 2022/7.25-7.31/592.分数加减运算.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=592 lang=javascript
+ *
+ * [592] 分数加减运算
+ */
+
+// @lc code=start
+/**
+ * @param {string} expression
+ * @return {string}
+ */
+var fractionAddition = function (expression) {
+  let res = new Fraction({ numerator: 0, denominator: 1 })
+  const isOperator = s => /[-+]/.test(s)
+  let operator = "+"
+  for (let i = 0, len = expression.length; i < len; ) {
+    let j = i + 1
+    while (!isOperator(expression[j]) && j < len) {
+      j++
+    }
+
+    const num = expression.slice(i, j)
+    const curr = new Fraction(num)
+    res = Fraction.computed(res, curr, operator)
+    operator = expression[j]
+    i = j + 1
+  }
+
+  if (res.numerator === 0) {
+    return `0/1`
+  } else {
+    const g = gcd(Math.abs(res.numerator), Math.abs(res.denominator))
+    res.numerator /= g
+    res.denominator /= g
+    return `${res.numerator}/${res.denominator}`
+  }
+}
+
+class Fraction {
+  constructor(obj) {
+    if (typeof obj === "string") {
+      const s = obj.split("/")
+      this.numerator = parseInt(s[0])
+      this.denominator = parseInt(s[1])
+    } else if (typeof obj === "object") {
+      this.numerator = obj.numerator
+      this.denominator = obj.denominator
+    }
+  }
+
+  static computed(f1, f2, operator) {
+    const numerator = new Function(
+      `return ${f1.numerator * f2.denominator} ${operator} ${
+        f2.numerator * f1.denominator
+      }`
+    )()
+    const denominator = f1.denominator * f2.denominator
+    return new Fraction({ numerator, denominator })
+  }
+}
+
+// 最大公约数
+function gcd(a, b) {
+  return b === 0 ? a : gcd(b, a % b)
+}
+
+// @lc code=end
+var expression = "-1/2+1/2"
+expression = "-1/2+1/2+1/3"
+expression = "1/3-1/2+1/9"
+
+var res = fractionAddition(expression)
+console.log(res)
