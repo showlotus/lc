29c7af593f7373e76eed5400065108b723e67749
showlotus <7826116+showlotus@user.noreply.gitee.com>
2022-08-21 18:16:57 +0800

    feat: 1455.检查单词是否为句中其他单词的前缀

---
+++ 2022/8.15-8.21/1455.检查单词是否为句中其他单词的前缀.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1455 lang=javascript
+ *
+ * [1455] 检查单词是否为句中其他单词的前缀
+ */
+
+// @lc code=start
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function (sentence, searchWord) {
+  const index = sentence.split(/\s/).findIndex(v => v.startsWith(searchWord));
+  return index === -1 ? -1 : index + 1;
+};
+// @lc code=end
+var sentence = "i love eating burger",
+  searchWord = "burg";
+(sentence = "this problem is an easy problem"), (searchWord = "pro");
+(sentence = "i am tired"), (searchWord = "you");
+console.log(isPrefixOfWord(sentence, searchWord));
