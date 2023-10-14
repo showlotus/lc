be56d28766133349e1f65b4860ca158a978eae40
showlotus <2257030399@qq.com>
2023-10-14 13:00:42 +0800

    136.只出现一次的数字 - 位运算 - O(N) O(1)

---
+++ 2023/10/136.只出现一次的数字.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let res = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    res ^= nums[i]
+  }
+  return res
+}
+// @lc code=end
+
+let nums = [1]
+const res = singleNumber(nums)
+console.log(res)
